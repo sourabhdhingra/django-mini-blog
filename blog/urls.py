cefdaa351bc9fad99b6ba52f86cb06dff5818eb9
sourabhdhingra <sourabhdhingra91@gmail.com>
2023-03-25 12:02:06 +0530

    - updated base_generic.html with links,\n- added ListView for bloggers and added template\n- added url for bloggers

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('bloggers', views.BloggerList.as_view(), name='bloggers'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+
+from . import models
 # from django.template import loader
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+
+class BloggerList(generic.ListView):
+    model = models.Blogger

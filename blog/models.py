cf63d7f6f08c35ac7e0ca59ad1eb2dd1d5b1561f
sourabhdhingra <sourabhdhingra91@gmail.com>
2023-03-25 11:09:45 +0530

    made Blogger list appear with names in admin site

--- blog/models.py
+++ blog/models.py
@@ -4,5 +4,8 @@ from django.db import models
 class Blogger(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
     
     

401a0ab596701bf76c281cd8b61e6ee78fe99042
sourabhdhingra <sourabhdhingra91@gmail.com>
2023-03-25 15:16:25 +0530

    added slug support scope

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ class Blogger(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     about = models.TextField(max_length=2000, null=True)
+    # slug = models.SlugField(default=str(f'{first_name}-{last_name}'), null=False)
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'

9aa6fb4810e157b6bd8277b8e1d1d5d27f0ca071
sourabhdhingra <sourabhdhingra91@gmail.com>
2023-03-26 12:23:33 +0530

    setting login redirect url to home

--- miniblog/settings.py
+++ miniblog/settings.py
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+## Redirect urls
+LOGIN_REDIRECT_URL = 'home'

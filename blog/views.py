9387ddc7a19bd4a38f17737dc2a279f40b7f2177
sourabhdhingra <sourabhdhingra91@gmail.com>
2023-03-26 12:52:41 +0530

    added LoginRequiredMixin to views other than home page

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,27 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
 from django.views import generic
-from django.contrib.auth.views import LoginView
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 from . import models
-# from django.template import loader
+
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 
-class BloggerList(generic.ListView):
+class BloggerList(LoginRequiredMixin, generic.ListView):
     model = models.Blogger
 
 
-class BloggerDetail(generic.DetailView):
+class BloggerDetail(LoginRequiredMixin, generic.DetailView):
     model = models.Blogger
 
 
-class BlogPostList(generic.ListView):
+class BlogPostList(LoginRequiredMixin, generic.ListView):
     model = models.BlogPost
 
 
-class BlogPostDetail(generic.DetailView):
+class BlogPostDetail(LoginRequiredMixin, generic.DetailView):
     model = models.BlogPost
 
 

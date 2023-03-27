73a7d564f4962ee6ef5f8d856815d24207661647
sourabhdhingra <sourabhdhingra91@gmail.com>
2023-03-27 16:37:00 +0530

    added StackedInline for Blogger inside UserAdmin\nadded link for creating a blogger from an existing user

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
-from . import models
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from blog import models
 
 # Register your models here.
 admin.site.register(models.Blogger)
 admin.site.register(models.BlogPost)
 admin.site.register(models.Comment)
+
+class BloggerInline(admin.StackedInline):
+    model = models.Blogger
+    can_delete = False
+    verbose_name_plural = 'blogger'
+
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (BloggerInline,)
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)

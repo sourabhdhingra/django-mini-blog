68ff37f779e95dc10fb4b880a581d1b2eaefcc8b
sourabhdhingra <sourabhdhingra91@gmail.com>
2023-04-10 11:09:35 +0530

    added custom logic to support the save of comment.slug field from admin too

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-from blog import models
+from . import models, utils
+from django.utils.text import slugify
+
 
 # Register your models here.
 admin.site.register(models.Blogger)
-admin.site.register(models.Comment)
 
 class BloggerInline(admin.StackedInline):
     model = models.Blogger
@@ -16,8 +17,16 @@ class BloggerInline(admin.StackedInline):
 class BlogPostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ['title']}
 
+class CommentAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ['content']}
+
+    def save_model(self, request, obj, form, change):
+        obj.slug = slugify(f"{obj.commentor.username}-{slugify(obj.content[:50])}-{utils.get_random_alphanumeric()}") 
+        return super().save_model(request, obj, form, change)
+
 
 admin.site.register(models.BlogPost, BlogPostAdmin)
+admin.site.register(models.Comment, CommentAdmin)
 
 
 # Define a new User admin
